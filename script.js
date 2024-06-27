cfabe8a5641d6224a15c680ba4c73e13cdf78849
BlairStChair <julia.z15@interia.pl>
2024-06-27 19:45:56 +0200

    Prepared elements of items added to list

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ btn.addEventListener("click", (e) => {
     let new_item = input.value;
     input.value = '';
 })
+
+let list_item = document.createElement("li");
+let span = document.createElement("span");
+let btn2 = document.createElement("button");
+
+list_item.appendChild(span);
+list_item.appendChild(btn2);
+
+span.textContent = new_item;
+btn2.textContent = "Delete";
