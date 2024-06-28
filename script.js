52cc253aa92e8bbb2ec474c6d1d4c24f246548fb
BlairStChair <julia.z15@interia.pl>
2024-06-28 17:29:25 +0200

    Added CSS styles

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ btn.addEventListener('click', () => {
 let list_item = document.createElement("li");
 let span = document.createElement("span");
 let btn2 = document.createElement("button");
+let checkbox= document.createElement("input")
+checkbox.setAttribute("type","checkbox");
+checkbox.setAttribute("id", "check");
+
+list_item.appendChild(checkbox);
 
 list_item.appendChild(span);
 span.textContent = new_item;
 
 list_item.appendChild(btn2);
 btn2.textContent = "Delete";
+
 
 list.appendChild(list_item);
 
